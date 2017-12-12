39ba5bb9de0be19344d6d1521bcf7db4acd5aa9c
Vincent Walkergit config --global user.email vincethewalker@gmail.com <vincethewalker@yahoo.co.uk>
2017-12-12 18:34:17 +0000

    Final configuration for s3

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,4 +1,12 @@
 class PictureUploader < CarrierWave::Uploader::Base
+  
+  # Set up for s3
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -36,9 +44,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_whitelist
+     %w(jpg jpeg gif png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
